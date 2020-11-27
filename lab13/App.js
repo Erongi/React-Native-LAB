edd886a136b8f78ff6aff786044c377d4bed3709
Erongi <40891924+Erongi@users.noreply.github.com>
2020-11-27 20:11:28 +0700

    Update App.js

--- lab13/App.js
+++ lab13/App.js
@@ -1,65 +1,69 @@
 import React, { useRef } from "react";
-import { Animated, PanResponder, StyleSheet, View } from "react-native";
-
+import { StyleSheet, View, Animated, PanResponder } from "react-native";
 export default function App() {
   const pan = useRef(new Animated.ValueXY()).current;
   const scale = useRef(new Animated.Value(1)).current;
-  const panResponder = PanResponder.create({
+  const responder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
     onPanResponderGrant: () => {
-      pan.setOffset({ x: pan.x._value, y: pan.y._value, });
-      pan.setValue({ x: 0, y: 0 }); },
-
-    onPanResponderMove: (evt, gestureState) => {
+      pan.setOffset({ x: pan.x._value, y: pan.y._value });
+      pan.setValue({ x: 0, y: 0 });
+    },
+    onPanResponderMove: (evt, gestureState) => {  
       const touches = evt.nativeEvent.touches;
-      if (touches.length >= 2) {
+      console.log("------------------------------------------");
+      console.log(touches);
+      console.log(touches.length);
+      if (touches.length === 1) {
+        pan.setValue({ x: gestureState.dx, y: gestureState.dy });
+      } else if (touches.length === 2) {
         let x1 = touches[0].locationX;
         let y1 = touches[0].locationY;
         let x2 = touches[1].locationX;
         let y2 = touches[1].locationY;
         const distance =
-          Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)) / 100;
+          Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)) / 100;
         Animated.spring(scale, {
           toValue: distance,
-          friction: 3,
+          friction: 4,
           useNativeDriver: false,
         }).start();
       }
-      if (touches.length = 1){
-        pan.setValue({ x: gestureState.dx, y: gestureState.dy });
-      }
     },
-
     onPanResponderRelease: () => {
       pan.flattenOffset();
       Animated.spring(scale, {
         toValue: 1,
-        friction: 3,
+        friction: 4,
         useNativeDriver: false,
       }).start();
     },
   });
-
+  
   return (
-    <View style={styles.container}>
+    <View style={styles.screen}>
       <Animated.Image
+        {...responder.panHandlers}
         source={require("./assets/logo.png")}
-        {...panResponder.panHandlers}
-        style={[{ transform: [{ scale: scale }] }, styles.box,pan.getLayout()]}
+        style={{
+          ...styles.logo,
+          ...pan.getLayout(),
+          transform: [{ scale: scale }],
+        }}
       />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    justifyContent: "center",
+    backgroundColor: "#fff",
     alignItems: "center",
+    justifyContent: "center",
   },
-  box: {
-    width: 100,
-    height: 100,
-    borderRadius: 4,
+  logo: {
+    width: 180,
+    height: 150,
   },
 });
